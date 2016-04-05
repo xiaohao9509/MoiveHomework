17c6fcc1d79f6058a5a7da8439fe646879e7220c
xiaohao9509 <305812387@qq.com>
2016-04-05 09:10:59 +0800

    111

--- app/src/main/java/com/nick/moivehomework/MainActivity.java
+++ app/src/main/java/com/nick/moivehomework/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
@@ -61,6 +62,11 @@ public class MainActivity extends AppCompatActivity implements HttpTask.Callback
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     public void onResponse(Movies movies) {
